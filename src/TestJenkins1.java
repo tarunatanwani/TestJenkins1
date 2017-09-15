186616720f401afb26bc349c89920c2d395a5a2d
Taruna T <taruna.tanwani@gmail.com>
2017-09-15 11:53:59 +0530

    Added TestJenkins1

---
+++ src/TestJenkins1.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author TARUNA
+ *
+ */
+public class TestJenkins1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i=0;i<=10;i++){
+			System.out.println("i : "+ i);
+		}
+
+	}
+
+}
